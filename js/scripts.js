a54439688b61e988df66d00ca352ea909cb3a7cb
TristanLobaugh <ttrader_atl@yahoo.com>
2017-05-02 10:57:57 -0400

    set up basic layout
    added Models and a Collection

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
+// Backbone Model
+const Blog = Backbone.Model.extend({
+	default: {
+		author: '',
+		title: '',
+		url: '',
+	}
+});
 
+// Backbone Collection
+
+const Blogs = Backbone.Collection.extend({});
+
+// instantiate 2 blogs
+const blog1 = new Blog({
+	author: 'Tristan',
+	title: 'Tristan\'s Blog',
+	url: 'http://tristansblog.com'
+});
+
+const blog2 = new Blog({
+	author: 'Chewie',
+	title: 'Chewie\'s Blog',
+	url: 'http://chewiesblog.com'
+});
+
+// instantiate a Collection
+const blogs = new Blogs([blog1, blog2]);
