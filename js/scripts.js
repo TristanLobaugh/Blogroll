9e30d066499ae0f927feecee78a24f9a78c71a8d
TristanLobaugh <ttrader_atl@yahoo.com>
2017-05-02 12:15:55 -0400

    added form and templates

--- js/scripts.js
+++ js/scripts.js
@@ -8,21 +8,67 @@ const Blog = Backbone.Model.extend({
 });
 
 // Backbone Collection
-
 const Blogs = Backbone.Collection.extend({});
 
 // instantiate 2 blogs
-const blog1 = new Blog({
-	author: 'Tristan',
-	title: 'Tristan\'s Blog',
-	url: 'http://tristansblog.com'
-});
+// const blog1 = new Blog({
+// 	author: 'Tristan',
+// 	title: 'Tristan\'s Blog',
+// 	url: 'http://tristansblog.com'
+// });
 
-const blog2 = new Blog({
-	author: 'Chewie',
-	title: 'Chewie\'s Blog',
-	url: 'http://chewiesblog.com'
-});
+// const blog2 = new Blog({
+// 	author: 'Chewie',
+// 	title: 'Chewie\'s Blog',
+// 	url: 'http://chewiesblog.com'
+// });
 
 // instantiate a Collection
-const blogs = new Blogs([blog1, blog2]);
+const blogs = new Blogs();
+
+// Backbone Views
+const BlogView = Backbone.View.extend({
+	model: new Blog(),
+	tagName: 'tr',
+	initialize() {
+		this.template = _.template($('.blogs-list-template').html());
+	},
+	render() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+});
+
+const BlogsView = Backbone.View.extend({
+	model: blogs,
+	el: $('.blogs-list'),
+	initialize() {
+		this.model.on('add', this.render, this);
+	},
+	render() {
+		const self = this;
+		this.$el.html('');
+		_.each(this.model.toArray(), blog => {
+			self.$el.append((new BlogView({ model: blog })).render().$el);
+		});
+		return this;
+	}
+});
+
+const blogsView = new BlogsView();
+
+$(document).ready(() => {
+	$('.add-blog').on('click', () => {
+		const blog = new Blog({
+			author: $('.author-input').val(),
+			title: $('.title-input').val(),
+			url: $('.url-input').val()
+		});
+		$('.author-input').val('');
+		$('.title-input').val('');
+		$('.url-input').val('');
+		console.log(blog.toJSON());
+		blogs.add(blog);
+	});
+});
+
